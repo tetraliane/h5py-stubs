0dc30ef31e95117d29cedd2f5bfc6fe7bedcf6c6
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2024-08-16 13:34:16 +0900

    Fix File.__init__()

--- h5py-stubs/_hl/files.pyi
+++ h5py-stubs/_hl/files.pyi
@@ -1,8 +1,9 @@
-from typing import Any
+from typing import Any, Literal
 
 from .group import Group
 
 class File(Group):
+    def __init__(self, name: str, mode: Literal["w", "r", "r+", "a", "w-"] = "r", **kwargs: Any)
     def close(self) -> None: ...
     def __enter__(self) -> File: ...
     def __exit__(self, *args: Any) -> None: ...
